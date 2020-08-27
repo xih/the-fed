9a2c8cac8b4826b9d9e96d945dca585e27bde7cd
xih <yxing@berkeley.edu>
2020-08-26 23:36:10 -0700

    add firebase createUserWithEmailAndPassword function to Sign up page

---
+++ src/Firebase/firebase.utils.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6chYa7YxajP57R6nFOpPoasrISjpVhw8",
+  authDomain: "flames-4cfe9.firebaseapp.com",
+  databaseURL: "https://flames-4cfe9.firebaseio.com",
+  projectId: "flames-4cfe9",
+  storageBucket: "flames-4cfe9.appspot.com",
+  messagingSenderId: "3968717937",
+  appId: "1:3968717937:web:e6696982629aa8054a77d0",
+  measurementId: "G-03RQM1LXZE"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
--- src/SignupPage/index.js
+++ src/SignupPage/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux'
 import { Form, Button, Container, Col } from 'react-bootstrap'
 import Navbar from '../components/Navbar'
+import { auth } from '../Firebase/firebase.utils'
+import { push } from 'connected-react-router'
 
-const LoginPage = () => {
+const SignupPage = () => {
+  const [email, setEmail] = useState('')
+  const [firstName, setfirstName] = useState('')
+  const [lastName, setlastName] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(email, firstName, lastName, password)
+    try {
+      console.log('hello')
+      let response = await auth.createUserWithEmailAndPassword(email, password)
+      console.log('response', response)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -12,25 +33,35 @@ const LoginPage = () => {
           <Form.Row>
             <Form.Group as={Col} controlId="formFirstName">
               <Form.Label>First name</Form.Label>
-              <Form.Control type="text" placeholder="Enter first name" />
+              <Form.Control
+                onChange={(e) => setfirstName(e.target.value)}
+                type="text" placeholder="Enter first name" />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formLastName">
               <Form.Label>Last name</Form.Label>
-              <Form.Control type="text" placeholder="Last name!" />
+              <Form.Control
+                onChange={(e) => setlastName(e.target.value)}
+                type="text" placeholder="Last name!" />
             </Form.Group>
           </Form.Row>
           <Form.Group controlId="formEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="email" />
+            <Form.Control
+              onChange={(e) => setEmail(e.target.value)}
+              type="email" placeholder="email" />
           </Form.Group>
-          
+
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              onChange={(e) => setPassword(e.target.value)}
+              type="password" placeholder="Password" />
           </Form.Group>
-          
-          <Button variant="primary" type="submit">
+
+          <Button
+            onClick={handleSubmit}
+            variant="primary" type="submit">
             Submit
           </Button>
         </Form>
@@ -39,4 +70,13 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+const mapStateToProps = null 
+
+const mapDispatchToProps = dispatch => ({
+  push: (url) => dispatch(push(url))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignupPage);
